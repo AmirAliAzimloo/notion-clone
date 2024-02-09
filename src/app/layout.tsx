c73f5b62cf85f24e15acb2af3468cae05195a73d
Amir Ali <amiraliazimloo123@gmail.com>
2024-02-10 01:59:08 +0330

    up to date

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import db from "@/lib/supabase/db";
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Notion-Clone",
+  description: "Notion Clone by Amir Ali Azimloo",
+};
+
+export default function RootLayout({ 
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  console.log(db)
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          >
+
+        {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
