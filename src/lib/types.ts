aa99cf4aa82219094326ac8ca1da200a2941cc19
Amir Ali <amiraliazimloo123@gmail.com>
2024-02-10 02:19:09 +0330

    last checking git

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,7 +4,4 @@ export const FormSchema = z.object({
   email: z.string().describe('Email').email({ message: 'Invalid Email' }),
   password: z.string().describe('Password').min(1, 'Password is required'),
 });
-<<<<<<< HEAD
  
-=======
->>>>>>> 4bf312325d0326fb35fccf6594cbbca6a7cfc182
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-<<<<<<< HEAD
 export function cn(...inputs: ClassValue[]) { 
   return twMerge(clsx(inputs))    
 } 
-=======
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
->>>>>>> 4bf312325d0326fb35fccf6594cbbca6a7cfc182
